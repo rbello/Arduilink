e183dbd38d24ec5c257161aaa4b2a09629cef401
R. BELLO <remi@evolya.fr>
2016-06-05 17:21:50 +0200

    Get fonctionnel

--- python-scripts/arduilink.py
+++ python-scripts/arduilink.py
@@ -61,11 +61,8 @@ if action == 'server' :
 
 #arduino = serial.Serial(serialPort, baudrate=serialBaudrate, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, timeout=1, xonxoff=0, rtscts=0)
 arduino = serial.Serial(serialPort, serialBaudrate)
-# Toggle DTR to reset Arduino
 arduino.setDTR(False)
-time.sleep(1)
-# toss any data already received, see
-# http://pyserial.sourceforge.net/pyserial_api.html#serial.Serial.flushInput
+time.sleep(.1)
 arduino.flushInput()
 arduino.setDTR(True)
 
