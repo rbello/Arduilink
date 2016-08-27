110d2f56c319db471d89cccae3b8bf4da69d97b2
R. BELLO <remi@evolya.fr>
2016-08-27 17:25:27 +0200

    Init

--- python-scripts/init.sh
+++ python-scripts/init.sh
@@ -6,17 +6,33 @@
 # PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 # @reboot   root    /var/www/html/plugins/script/core/ressources/init.sh
 
-
-sudo chmod 777 /dev/ttyUSB0
-
 cd /var/www/html/plugins/script/core/ressources
 
-sudo rm server.log
-sudo rm client.log
+sudo chown jeedom:www-data client.log
+sudo chown jeedom:www-data server.log
+sudo chown jeedom:www-data arduilink_server.py
+sudo chown jeedom:www-data arduilink_client.py
 
-sudo ./arduilink_server.py --port 1008 > ./server.log &
+pathToArduino=/dev/ttyUSB0
+if [ ! -f pathToArduino ]; then
+	echo $pathToArduino
+	pathToArduino=/dev/ttyACM0
+    if [ ! -f pathToArduino ]; then
+		echo $pathToArduino
+		echo "Error: Arduino is not connected"
+		echo "Error: Arduino is not connected" > ./server.log
+		exit 2
+	fi
+fi
+echo $pathToArduino
+exit 0
+
+sudo chmod 777 $pathToArduino
+
+# Server startup
+sudo ./arduilink_server.py --port 1008 --file $pathToArduino > ./server.log 2>&1 &
 
 sleep 5
 
 # Capteur EDF arduino #1 vers jeedom ID #13
-sudo ./arduilink_client.py --port 1008 --mode watch --target 1 --run "php ./jeedom_push.php 13" > ./client.log &
+sudo ./arduilink_client.py --port 1008 --mode watch --target 2:1 --run "php ./jeedom_push.php 13" > ./client.log 2>&1 &
