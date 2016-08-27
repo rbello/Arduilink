ba2c3577a324861146f9f8e90c68585ae60e8390
R. BELLO <remi@evolya.fr>
2016-08-27 17:52:01 +0200

    Fix init + create a php file to list all availables commands on devices

--- python-scripts/init.sh
+++ python-scripts/init.sh
@@ -14,18 +14,14 @@ sudo chown jeedom:www-data arduilink_server.py
 sudo chown jeedom:www-data arduilink_client.py
 
 pathToArduino=/dev/ttyUSB0
-if [ ! -f pathToArduino ]; then
-	echo $pathToArduino
+if [ ! -e $pathToArduino ]; then
 	pathToArduino=/dev/ttyACM0
-    if [ ! -f pathToArduino ]; then
-		echo $pathToArduino
+    if [ ! -e $pathToArduino ]; then
 		echo "Error: Arduino is not connected"
 		echo "Error: Arduino is not connected" > ./server.log
 		exit 2
 	fi
 fi
-echo $pathToArduino
-exit 0
 
 sudo chmod 777 $pathToArduino
 
@@ -34,5 +30,8 @@ sudo ./arduilink_server.py --port 1008 --file $pathToArduino > ./server.log 2>&1
 
 sleep 5
 
-# Capteur EDF arduino #1 vers jeedom ID #13
+# Capteur de conso
 sudo ./arduilink_client.py --port 1008 --mode watch --target 2:1 --run "php ./jeedom_push.php 13" > ./client.log 2>&1 &
+# Capteur de température
+sudo ./arduilink_client.py --port 1008 --mode watch --target 2:2 --run "php ./jeedom_push.php 6" > ./client.log 2>&1 &
+sudo ./arduilink_client.py --port 1008 --mode watch --target 2:3 --run "php ./jeedom_push.php 7" > ./client.log 2>&1 &
---
+++ python-scripts/jeedom_list.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once '../../../../core/php/core.inc.php';
+
+$i = 0;
+
+// Fetch equipments
+foreach (eqLogic::byType('script') as $eq) {
+	// Fetch commands
+	foreach ($eq->getCmd('info') as $cmd) {
+		echo "Device='{$eq->getName()}' Command='{$cmd->getName()}' ID={$cmd->getId()}\n";
+		$i++;
+	}
+}
+
+echo "Found: {$i} command(s)\n"
+
+?>
